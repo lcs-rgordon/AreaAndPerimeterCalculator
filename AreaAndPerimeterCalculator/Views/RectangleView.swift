61ffc696bdd94a521b29a685586d2253ea0b853c
Russell Gordon <rgordon@lcs.on.ca>
2023-01-17 11:53:00 -0500

    Added properties to store length and width; calculated area; made a static layout.

--- AreaAndPerimeterCalculator/Views/RectangleView.swift
+++ AreaAndPerimeterCalculator/Views/RectangleView.swift
@@ -10,8 +10,13 @@ import SwiftUI
 struct RectangleView: View {
     
     // MARK: Stored properties
+    let length: Double = 7
+    let width: Double = 5
     
     // MARK: Computed properties
+    var area: Double {
+        return length * width
+    }
     
     // Shows our user interface
     var body: some View {
@@ -25,6 +30,29 @@ struct RectangleView: View {
                 
                 Spacer()
             }
+            
+            // Length
+            Text("Length")
+                .font(.title2)
+                .bold()
+            // Use string interpolation \() to display length
+            Text("\(length)")
+
+            // Width
+            Text("Width")
+                .font(.title2)
+                .bold()
+            // Use string interpolation \() to display width
+            Text("\(width)")
+
+            // Area
+            Text("Area")
+                .font(.title2)
+                .bold()
+            // Use string interpolation \() to display area
+            Text("\(area)")
+
+            
             
             Spacer()
             
